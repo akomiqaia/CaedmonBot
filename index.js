37ab60cb467053fdc4f4cfc04c000e72f9403d92
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2018-11-24 16:04:34 +0000

    trying new ways

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-'use strict'
- 
-const Telegram = require('telegram-node-bot')
-const TelegramBaseController = Telegram.TelegramBaseController
-const TextCommand = Telegram.TextCommand
-const tg = new Telegram.Telegram('686681991:AAF7UZokmM8hfL4QwlYjndSllBefphgePfM')
- 
+"use strict";
+
+const Telegram = require("telegram-node-bot");
+const TelegramBaseController = Telegram.TelegramBaseController;
+const TextCommand = Telegram.TextCommand;
+const tg = new Telegram.Telegram(
+  "686681991:AAF7UZokmM8hfL4QwlYjndSllBefphgePfM"
+);
+
 class PingController extends TelegramBaseController {
-    /**
-     * @param {Scope} $ 
-     */
-    pingHandler($) {
-        $.sendMessage('pong')
-    }
- 
-    get routes() {
-        return {
-            'pingCommand': 'pingHandler'
-        }
-    }
+  /**
+   * @param {Scope} $
+   */
+  pingHandler($) {
+    $.sendMessage("png");
+  }
+
+  get routes() {
+    return {
+      pingCommand: "pingHandler"
+    };
+  }
 }
- 
+
+tg.router.when(new TextCommand("hello", "pingCommand"), new PingController());
+
 tg.router
-    .when(
-        new TextCommand('hello', 'pingCommand'),
-        new PingController()
-    )
+  .when(new TextCommand("/start", "startCommand"), new StartController())
+  .when(new TextCommand("/stop", "stopCommand"), new StopController())
+  .when(new TextCommand("/restart", "restartCommand"), new RestartController())
+  .otherwise(new OtherwiseController());
